2265cbd4520a49cd3bc216133f229a4fcde65521
Ramadan, Omar <oramadan@iupui.edu>
2019-11-30 19:11:59 -0500

    Must install npm and necessary packages in order to run app

---
+++ app.js
@@ -0,0 +1,126 @@
+//App setup
+var express = require("express");
+var request = require("request");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var expressSanitizer = require("express-sanitizer");
+var dotenv = require("dotenv");
+var port = parseInt(process.env.PORT,10) || 3000;
+app = express();
+
+//For secure access to DB
+dotenv.config();
+var url = process.env.MONGOLAB_URI;
+
+//DB setup
+mongoose.connect(url,
+{ useNewUrlParser: true }).then(() => {
+    console.log("Connected to DB");
+}).catch(err => {
+    console.log("ERROR", err.message);
+}); 
+
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
+
+//DB initialisation
+var transactionSchema = new mongoose.Schema({
+    name: String,
+    amount: Number,
+    category: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Transaction = mongoose.model("Transaction", transactionSchema);
+
+//Home Page (redirects to transactions page)
+app.get("/",function(req,res){
+    res.redirect("/transactions");
+});
+
+
+//index page
+app.get("/transactions",function(req,res){
+    Transaction.find({}, function(err,transactions){
+        if (err){
+            console.log("ERROR!");
+        }
+        else{
+            res.render("index",{transactions: transactions});  
+        }
+    });
+});
+
+//new
+app.get("/transactions/new",function(req,res){
+    res.render("new");
+});
+
+//create route
+app.post("/transactions",function(req,res){
+    //Make sure user input doesn't have script tags (may be malicious)
+    req.body.transaction.body = req.sanitize(req.body.transaction.name);
+    //create transaction
+    Transaction.create(req.body.transaction, function(err,newTransaction){
+        if (err)
+            res.render("new");
+        //redirect to index
+        else
+        res.redirect("/transactions");
+    });
+ 
+});
+
+//Show route
+app.get("/transactions/:id",function(req,res){
+    Transaction.findById(req.params.id, function(err, foundTransaction){
+        if (err)
+            res.redirect("/transactions");
+        else
+            res.render("show",{transaction: foundTransaction});
+    });
+});
+
+//Edit route
+app.get("/transactions/:id/edit",function(req,res){
+    Transaction.findById(req.params.id, function(err, foundTransaction){
+        if (err)
+            res.redirect("/transactions");
+        else
+            res.render("edit",{transaction: foundTransaction});
+    });
+});
+
+
+//Update route
+app.put("/transactions/:id",function(req,res){
+    //Make sure user input doesn't have script tags (may be malicious)
+    req.body.transaction.body = req.sanitize(req.body.transaction.body);
+    Transaction.findByIdAndUpdate(req.params.id, req.body.transaction ,function(err, updatedTransaction){
+        if (err)
+            res.redirect("/transactions");
+        else
+            res.redirect("/transactions/" + req.params.id);
+    });
+});
+
+//Delete route
+app.delete("/transactions/:id",function(req,res){
+    Transaction.findByIdAndRemove(req.params.id, function(err){
+        if (err)
+            res.redirect("/transactions");
+        else
+            res.redirect("/transactions");
+    });
+});
+
+app.listen(port, function(){
+    console.log("App started on " + port);
+}); 
